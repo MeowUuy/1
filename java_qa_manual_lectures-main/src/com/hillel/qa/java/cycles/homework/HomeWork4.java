3442785d926f595bc7966ada059faee412ba4446
MeowUuy <games2014ullia@gmail.com>
2023-04-06 12:44:43 +0300

    А

--- java_qa_manual_lectures-main/src/com/hillel/qa/java/cycles/homework/HomeWork4.java
+++ java_qa_manual_lectures-main/src/com/hillel/qa/java/cycles/homework/HomeWork4.java
@@ -32,7 +32,7 @@ public class HomeWork4 {
      * </p>
      */
     public static void main(String[] args) {
-        int n = 8;
+        int n = 5;
         for (int i = 0; i <= n; i++) {
             for (int j = i; j > 0; j--) {
                 System.out.print(": ");
