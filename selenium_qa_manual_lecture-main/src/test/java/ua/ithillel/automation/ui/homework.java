290c298ac0a4a81176efa1f86e2911cf806e8b6a
MeowUuy <games2014ullia@gmail.com>
2023-04-09 19:58:33 +0300

    код працює

--- selenium_qa_manual_lecture-main/src/test/java/ua/ithillel/automation/ui/homework.java
+++ selenium_qa_manual_lecture-main/src/test/java/ua/ithillel/automation/ui/homework.java
@@ -1,10 +1,9 @@
 package ua.ithillel.automation.ui;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -29,9 +28,37 @@ public class homework {
     }
 
     @Test
-    public void qAutoEnd2End(){
+    public void qAutoEnd2End() throws InterruptedException {
         driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
-        //driver.findElement()
+        Thread.sleep(500);
+        driver.findElement(By.cssSelector(".hero-descriptor_btn")).click();
+        Thread.sleep(500);
+        driver.findElement(By.id("signupName")).sendKeys("Test");
+        driver.findElement(By.id("signupLastName")).sendKeys("Test");
+        driver.findElement(By.id("signupEmail")).sendKeys("Test221@gmail.com");
+        driver.findElement(By.id("signupPassword")).sendKeys("Test12345");
+        driver.findElement(By.id("signupRepeatPassword")).sendKeys("Test12345");
+        driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".sidebar_btn-group .sidebar_btn")).click();
+        WebElement profilename = driver.findElement(By.cssSelector(".profile_name"));
+        Assert.assertEquals("Incorrect button name ", "Test Test", profilename.getText());
+        driver.findElement(By.cssSelector(".sidebar_btn")).click();
+        driver.findElement(By.cssSelector(".btn-primary")).click();
+        driver.findElement(By.id("addCarBrand")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("select  option:nth-child(4)")).click();
+        driver.findElement(By.id("addCarMileage")).sendKeys("1");
+        driver.findElement(By.cssSelector("body > ngb-modal-window > div > div > app-add-car-modal > div.modal-footer.d-flex.justify-content-end > button.btn.btn-primary")).click();
+        driver.findElement(By.cssSelector("body > app-root > app-global-layout > div > div > div > app-panel-layout > div > div > div > div.col-lg-9.main-wrapper > div > app-garage > div > div.panel-page_content > div > ul > li > app-car > div > div.car-heading.d-flex.flex-column.flex-sm-row.justify-content-sm-between > div.car_actions > button.car_add-expense.btn.btn-success")).click();
+        driver.findElement(By.id("addExpenseLiters")).sendKeys("12");
+        driver.findElement(By.id("addExpenseTotalCost")).sendKeys("24");
+        driver.findElement(By.id("addExpenseMileage")).sendKeys("3");
+        driver.findElement(By.cssSelector("body > ngb-modal-window > div > div > app-add-expense-modal > div.modal-footer.d-flex.justify-content-end > button.btn.btn-primary")).click();
+        driver.findElement(By.cssSelector("body > app-root > app-global-layout > div > div > div > app-panel-layout > div > div > div > div.col-3.d-none.d-lg-block.sidebar-wrapper > nav > div > a:nth-child(2)")).click();
+        driver.findElement(By.cssSelector("body > app-root > app-global-layout > div > div > div > app-panel-layout > div > div > div > div.col-lg-9.main-wrapper > div > app-settings > div > div.panel-page_content > div > div.user-settings_item.-form.-remove-account > div > button")).click();
+        driver.findElement(By.cssSelector("body > ngb-modal-window > div > div > app-remove-account-modal > div.modal-footer.d-flex.justify-content-end > button.btn.btn-danger")).click();
+        Thread.sleep(1000);
     }
 
     @After
